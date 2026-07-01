0cb5716c9257497477307221236b4e5504bb036c
chrystian coutinho <chrystiancsv@gmail.com>
2026-07-01 16:52:38 -0300

    primeiro commit JS

---
+++ js-simples/index.js
@@ -0,0 +1,34 @@
+// window.prompt("HelloWorld")
+
+// console.log(456);
+// console.log("teste");
+
+// Tipos primitivos no JS= String, number, boolean, undefined, null, simble
+
+
+// variaveis n podem ter espaco entre si, pode usar CamelCase
+// atribuindo valores
+//const é uma variavel que nao muda, deve-se manter sempre a mesma, não se varia
+// diferença de var e let é o escopo, let e const só funcionam em escopo local(de bloco), a var funciona tambem fora
+
+// let cnkq = "hbk"
+
+// var vwrv = "cwmjk"
+
+// const qveq = "mvklq"
+
+// function wvrbw(){     
+    
+// } 
+
+// let retornoDoUsuario = alert("deseja continuar?")
+
+// console.log(retornoDoUsuario);
+
+
+const nome = "cvwhjvb"
+
+console.log(nome);
+
+
+ 
