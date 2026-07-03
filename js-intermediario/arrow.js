532a4f452f61466d2728a52b2b54012f4c66584d
Chrystian Coutinho <chrystiancsv@gmail.com>
2026-07-02 23:59:19 -0300

    arrow functions

---
+++ js-intermediario/arrow.js
@@ -0,0 +1,26 @@
+let somar = function(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+console.log(somar(10, 20));                 
+
+
+let soma = (numero1, numero2) => {
+    return numero1 + numero2;
+}
+
+console.log(soma(15, 20));       
+
+const destino = nome =>{
+    console.log('parabens' + nome); // ou console.log(`parabens ${nome}`)
+    
+}
+
+destino('joao');
+
+
+let somase = (numero1, numero2) => numero1 + numero2;
+
+console.log(somase(5,7));
+
+    
---
+++ js-intermediario/funcao_anonima.js
@@ -0,0 +1,20 @@
+// function name(params) {
+    
+// }
+
+// let teste = function (params) {
+
+// }
+
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+
+let resultado = somar(10, 20);
+console.log(resultado);
+
+// let somar = function (numero1, numero2) {
+//     return numero1 + numero2;
+// }
+
+// console.log(somar(10, 20));
